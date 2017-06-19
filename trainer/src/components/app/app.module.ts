4bdb556e1cdc2b5ba87e79df8cff642f25f838d7
Garry.X.Gu (tech.usca11.Newegg) 22284 <gg11@buyabs.corp>
2017-06-19 16:01:41 -0700

    updated.

--- trainer/src/components/app/app.module.ts
+++ trainer/src/components/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { TrainerAppComponent }  from './app.component';
+//import { WorkoutRunnerComponent} from '../workout-runner/workout-runner.component';
+import {WorkoutRunnerModule} from '../workout-runner/workout-runner.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ TrainerAppComponent ],
+  imports:      [ BrowserModule, WorkoutRunnerModule],
+  declarations: [ TrainerAppComponent],
   bootstrap:    [ TrainerAppComponent ]
 })
 export class AppModule { }
